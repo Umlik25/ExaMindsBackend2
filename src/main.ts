6cd9a6c8658514da4c73515cd2a1ad8240cabfce
Umlik25 <chingizxzx@mail.ru>
2024-08-21 20:06:10 +0200

    Добавил поддержку CORS в приложение

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'http://localhost:8080', // Разрешить запросы с http://localhost:8080
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Разрешить отправку cookie
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
