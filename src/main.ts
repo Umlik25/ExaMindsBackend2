fafe9f0aff531a1663c86685770aae691eb89246
Umlik25 <chingizxzx@mail.ru>
2024-08-23 21:09:47 +0200

    Changed CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://elegant-taiyaki-9dfe8f.netlify.app',
+    origin: ['https://elegant-taiyaki-9dfe8f.netlify.app', 'examinds.az'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Разрешить отправку cookie
     allowedHeaders: 'Content-Type, Authorization',
